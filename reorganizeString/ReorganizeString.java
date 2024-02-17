2b46d97f79475ce05b250e9fb758b427ccc16d1a
arshali2774 <arshali.763z@gmail.com>
2024-02-17 07:56:03 +0530

    new

--- reorganizeString/ReorganizeString.java
+++ reorganizeString/ReorganizeString.java
@@ -2,10 +2,14 @@ package reorganizeString;
 
 /**
  * ReorganizeString
+ * priority queue is used to make the time complexity nlogn
  */
 public class ReorganizeString {
 
   public static void main(String[] args) {
     String s1 = "aab", s2 = "aaab";
+  }
+  public static String reorganize(String s){
+
   }
 }
