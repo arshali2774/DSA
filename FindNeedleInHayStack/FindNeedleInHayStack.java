1c40ed31bed60c118b1c08f576ec92862654b1de
arshali2774 <arshali.763z@gmail.com>
2024-03-30 02:09:39 +0530

    adas

---
+++ FindNeedleInHayStack/FindNeedleInHayStack.java
@@ -0,0 +1,14 @@
+
+public class FindNeedleInHayStack {
+
+  public static void main(String[] args) {
+    String haystack1 = "sadbutsad", needle1 = "sad";
+    String haystack2 = "leetcode", needle2 = "leeto";
+  }
+
+  public static int findTheNeedle() {
+    int ans = -1;
+
+    return ans;
+  }
+}
