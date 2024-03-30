452624cd57d2fce2e8cf6f5e3a17722affd40d7a
arshali2774 <arshali.763z@gmail.com>
2024-03-31 03:15:59 +0530

    remove all duplicates from string 2

--- FindNeedleInHayStack/FindNeedleInHayStack.java
+++ FindNeedleInHayStack/FindNeedleInHayStack.java
@@ -2,13 +2,28 @@
 public class FindNeedleInHayStack {
 
   public static void main(String[] args) {
-    String haystack1 = "sadbutsad", needle1 = "sad";
+    String haystack1 = "butsad", needle1 = "sad";
     String haystack2 = "leetcode", needle2 = "leeto";
+    System.out.println(findTheNeedle(haystack1, needle1));
+    System.out.println(findTheNeedle(haystack2, needle2));
   }
 
-  public static int findTheNeedle() {
+  public static int findTheNeedle(String haystack, String needle) {
     int ans = -1;
+    int n = haystack.length();
+    int m = needle.length();
 
+    for (int i = 0; i <= n - m; i++) {
+      for (int j = 0; j < m; j++) {
+        if (needle.charAt(j) != haystack.charAt(i + j)) {
+          break;
+        }
+        if (j == m - 1) {
+          ans = i;
+          return ans;
+        }
+      }
+    }
     return ans;
   }
 }
